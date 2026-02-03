e38a6080c24b43f2d6fb05d51c538bdbaeea8e41
fs1148143-alt <fs1148143@gmail.com>
2026-02-03 22:20:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ const question = document.getElementById("question");
 const message = document.getElementById("message");
 const buttons = document.querySelector(".buttons");
 
-noBtn.addEventListener("touchstart", moveButton); // Mobile/Tablet ke liye
+// Tablet/Mobile ke liye touch fix
+noBtn.addEventListener("touchstart", (e) => {
+    e.preventDefault(); // Click hone se rokhne ke liye
+    moveButton();
+});
+
 noBtn.addEventListener("mouseover", moveButton);
 
 function moveButton() {
+    // Button ko random jagah bhejne ke liye
     const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
     const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
-    noBtn.style.left = ${x}px;
-    noBtn.style.top = ${y}px;
+    
+    noBtn.style.position = "fixed"; // Isse screen par kahin bhi ja sakega
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
 }
 
 yesBtn.addEventListener("click", () => {
+    document.querySelector('.buttons').style.display = "none";
     question.style.display = "none";
-    buttons.style.display = "none";
     message.classList.remove("hidden");
 });
