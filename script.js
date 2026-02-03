c6c7a0c290b89928e64d42ab2642bb8f09574228
fs1148143-alt <fs1148143@gmail.com>
2026-02-03 22:29:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
-const question = document.getElementById("question");
+const mainCard = document.getElementById("main-card");
 const message = document.getElementById("message");
-const buttons = document.querySelector(".buttons");
-
-// Tablet/Mobile ke liye touch fix
-noBtn.addEventListener("touchstart", (e) => {
-    e.preventDefault(); // Click hone se rokhne ke liye
-    moveButton();
-});
-
-noBtn.addEventListener("mouseover", moveButton);
 
 function moveButton() {
-    // Button ko random jagah bhejne ke liye
     const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
     const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
-    
-    noBtn.style.position = "fixed"; // Isse screen par kahin bhi ja sakega
     noBtn.style.left = x + "px";
     noBtn.style.top = y + "px";
 }
 
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    moveButton();
+});
+
 yesBtn.addEventListener("click", () => {
-    document.querySelector('.buttons').style.display = "none";
-    question.style.display = "none";
+    mainCard.classList.add("hidden");
     message.classList.remove("hidden");
 });
