5da5c43e121e674ba8729b59bc4159416af935de
fs1148143-alt <fs1148143@gmail.com>
2026-02-03 22:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ const mainCard = document.getElementById("main-card");
 const message = document.getElementById("message");
 
 function moveButton() {
-    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
-    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
-    noBtn.style.left = x + "px";
-    noBtn.style.top = y + "px";
+    // Isse button screen ke kinaro se bahar nahi jayega
+    const padding = 50;
+    const maxX = window.innerWidth - noBtn.offsetWidth - padding;
+    const maxY = window.innerHeight - noBtn.offsetHeight - padding;
+    
+    const randomX = Math.floor(Math.random() * maxX) + padding/2;
+    const randomY = Math.floor(Math.random() * maxY) + padding/2;
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
 }
 
-noBtn.addEventListener("mouseover", moveButton);
+// Mobile touch aur Mouse dono ke liye
 noBtn.addEventListener("touchstart", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Button click na ho jaye
     moveButton();
 });
+noBtn.addEventListener("mouseover", moveButton);
 
 yesBtn.addEventListener("click", () => {
     mainCard.classList.add("hidden");
